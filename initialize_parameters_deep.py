12ad6b8dd1991f522612de1fb2faf7d8c31a1afa
PolarisMurray <mikemilk433@gmail.com>
2026-03-21 00:17:54 -0400

    parameter_initialization

---
+++ initialize_parameters_deep.py
@@ -0,0 +1,26 @@
+import numpy as np
+def initialize_parameters_deep(layer_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the dimenstions of each layer
+    in our network
+
+    Returns:
+    parameters -- python dictionary containing parameters "W1", 
+    "b1", ..., "WL", "bL":
+
+    Wl -- weight matrix of shape (layer_dims[l], layer_dims[l-1])
+    bl -- bias vector of shape (layer_dims)
+    """
+    np.rando.seed(3)
+
+    parameters = {}
+
+    l = len(layer_dims) # number of layers in the network
+
+    for i in range(1, l):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) * 0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+    
+    return parameters
+
